e47d700842604b53226638b4a830d5ee22e949ff
MK-Sohan <mksohan786@gmail.com>
2022-07-26 01:04:24 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewar
+app.use(cors());
+app.use(express.json());
+
+async function run() {
+  try {
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Eventy server is running");
+});
+
+app.listen(port, () => {
+  console.log("Listning to port", port);
+});
