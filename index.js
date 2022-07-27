479f67fe4b51aa0d9d5e29c844b6221355bd3f89
zubayer-salehin <freelancerzubear@gmail.com>
2022-07-27 22:58:54 +0600

    eventDetailsPageServerWork

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { application } = require("express");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,10 +19,43 @@ async function run() {
   try {
     await client.connect()
     const allServiceCollection = client.db("eventy-data-collection").collection("all-service");
+    const allVenueCollection = client.db("eventy-data-collection").collection("all-venue");
     const allReviewCollection = client.db("eventy-data-collection").collection("all-review");
+    const selectVenuCollection = client.db("eventy-data-collection").collection("select-venu");
 
+    app.get("/allservices", async (req, res) => {
+      const services = await allServiceCollection.find().toArray();
+      res.send(services);
+    })
 
-    
+    app.get("/allvenues", async (req, res) => {
+      const venues = await allVenueCollection.find().toArray();
+      res.send(venues);
+    })
+
+    app.get("/allservices/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await allServiceCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.get("/selectVenu", async (req, res) => {
+      const venu = await selectVenuCollection.find().toArray();
+      res.send(venu);
+    })
+
+    app.post("/venuInsert", async (req, res) => {
+      const selectVenu = req.body;
+      const venuCount = await selectVenuCollection.find().toArray();
+      if (venuCount.length) {
+        res.send({ acknowledged: false });
+      } else {
+        const venuPost = await selectVenuCollection.insertOne(selectVenu);
+        res.send(venuPost);
+      }
+    })
+
     app.post('/post-review', async (req, res) => {
       const postReview = await allReviewCollection.insertOne(req.body)
       res.send(postReview)
